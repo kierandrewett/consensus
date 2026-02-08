6a89c035e86131789956b91282343c3535a71587
kierandrewett <kieran@drewett.dev>
2026-02-08 12:43:20 +0000

    feat(admin): add admin creation with temp password

--- src/controllers/admin/AdminUserController.ts
+++ src/controllers/admin/AdminUserController.ts
@@ -1,5 +1,11 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { AdminRepository } from '../../repositories/AdminRepository';
+import { generatePassword } from '../../utils/password';
+import { hashPassword } from '../../utils/password';
+
+export interface CreateAdminRequest {
+    username: string;
+}
 
 export class AdminUserController {
     constructor(private adminRepo: AdminRepository) {}
@@ -14,5 +20,36 @@ export class AdminUserController {
 
         const admins = this.adminRepo.findAll();
         return reply.view('admin/users.ejs', { admins });
+    }
+
+    /**
+     * Create new admin
+     */
+    async createAdmin(
+        request: FastifyRequest<{ Body: CreateAdminRequest }>,
+        reply: FastifyReply
+    ): Promise<void> {
+        if (!request.session.get('isAdmin')) {
+            return reply.status(401).send({ error: 'Unauthorized' });
+        }
+
+        const { username } = request.body;
+        const tempPassword = generatePassword();
+        const hashedPassword = await hashPassword(tempPassword);
+
+        try {
+            this.adminRepo.create({
+                username,
+                passwordHash: hashedPassword,
+                mustChangePassword: true
+            });
+
+            return reply.view('admin/user-created.ejs', {
+                username,
+                temporaryPassword: tempPassword
+            });
+        } catch (error: any) {
+            return reply.status(400).send({ error: error.message });
+        }
     }
 }
