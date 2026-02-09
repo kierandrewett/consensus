cbecc512d7b7c67c22feabc0e2db8a6470230a06
kierandrewett <kieran@drewett.dev>
2026-02-09 02:09:16 +0000

    chore: add admin controller barrel exports

---
+++ src/controllers/admin/index.ts
@@ -0,0 +1,6 @@
+export { AdminAuthController } from './AdminAuthController';
+export { AdminDashboardController } from './AdminDashboardController';
+export { AdminElectionController } from './AdminElectionController';
+export { AdminVoterController } from './AdminVoterController';
+export { AdminUserController } from './AdminUserController';
+export { AdminSettingsController } from './AdminSettingsController';
