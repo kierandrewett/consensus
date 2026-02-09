2cdb7b764a083405698cb77c37bf725606581f39
kierandrewett <kieran@drewett.dev>
2026-02-09 19:31:25 +0000

    chore: add main controllers barrel export

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export { ElectionController } from './ElectionController';
+export { VoterController } from './VoterController';
+export { VotingController } from './VotingController';
+export * from './admin';
